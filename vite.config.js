17d4c5b30aeac52381263b90bd7cb1061807eae4
desigggner <132071682+desigggner@users.noreply.github.com>
2026-03-16 13:48:09 +0300

    Исправить переход на страницу Менеджмента

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        management: fileURLToPath(new URL('./management.html', import.meta.url)),
+      },
+    },
+  },
+});
